b4698a10bb767ebc19b3c47dee9c25c160a88e5c
Sakuntha200 <prasannaruwan340@gmail.com>
2024-11-10 15:32:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "94723873153"
+global.ownername = "Saku"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '94723873153'  //creator number
+global.ownername = 'Saku' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
