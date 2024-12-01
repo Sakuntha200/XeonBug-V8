6ea34dc26860908f833a466d7752db92ee629e93
Sakuntha200 <prasannaruwan340@gmail.com>
2024-12-01 16:55:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,8 +35,8 @@ global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
 global.autorecordtype = true  //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
